f329893bb5f24106d95bb4785ac32df38bf63af4
7439986 <jennifer.kulich@mines.sdsmt.edu>
2019-03-18 12:37:42 -0600

    Tried writing code so snake won't run into self.

--- player.cpp
+++ player.cpp
@@ -16,28 +16,61 @@ std::pair<int, int> getLocation(const int *grid, int value)
 
 // Make a move that approaches the food.  Does not try to avoid
 // obstacles (like its own tail)
+//called in driver.cpp
 ValidMove Player::makeMove(const Playfield *pf)
 {
    const int *grid = pf->getGrid();
    std::pair<int, int> head = getLocation(grid, HEAD_VALUE);
    std::pair<int, int> food = getLocation(grid, FOOD_VALUE);
 
+   //just trying to move the snake
+   //return DOWN;
+
    int dX = head.first - food.first;
    int dY = head.second - food.second;
 
+
+//TODO: check if there are obstacles and move
    if (dX == 0) // In the right column
    {
       if (dY > 0) // Above the food
-         return DOWN;
+      {
+         //check if moving DOWN would move it onto itself
+         if(grid[head.second + 1] != 0)
+            return LEFT;
+         else
+            return DOWN;
+      }
+         //return DOWN;
       else
-         return UP;
+      {
+         if(grid[head.second - 1] != 0)
+            return LEFT;
+         else  
+            return UP;
+      }
+        // return UP;
    }
    else // Not in the right column
    {
       if (dX > 0) // I'm to the right of the food
-         return LEFT;
+      {
+         //check if moving LEFT would moe it onto itself
+         if(grid[head.first - 1] != 0)
+            return UP;
+         else 
+            return LEFT;
+      }
+         //return LEFT;
       else  // I'm to the left of the food
-         return RIGHT;
+      {
+         if(grid[head.first + 1] != 0)
+            return UP;
+         else  
+            return RIGHT;
+      }
+         //return RIGHT;
    }      
    return NONE;
+   
 }
