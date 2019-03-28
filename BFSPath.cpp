94c75dc8dceccbc2e6ac18f9198327749264e0e9
Jennifer Kulich <jennifer.kulich@mines.sdsmt.edu>
2019-03-27 18:44:26 -0600

    Changed the constructor to set the headspot to s. It's not going into an infinite loop now.

--- BFSPath.cpp
+++ BFSPath.cpp
@@ -1,7 +1,8 @@
 #include "BFSPath.h"
 
-BFSPaths::BFSPaths(Graph *G, int s)
+BFSPaths::BFSPaths(Graph *G, int start)
 {
+  s = start;
   bfs(G,s);
 }
 
@@ -15,9 +16,20 @@ std::list<int> BFSPaths::PathTo(int v)
   std::list<int>path;
   if(!hasPath(v))
     return path;
+    std::cout << "PathTo 1" << std::endl;
   //GETS HUNG UP HERE
   for(int x = v; x != s; x = info[x].prev)
+  {
+    std::cout<< std::endl <<std::endl;
+    std::cout << "x: " << x << std::endl;
+    std::cout << "s: " << s << std::endl;
+    std::cout << "v: " << v <<std::endl;
+    std::cout << "info[x].prev: " << info[x].prev << std::endl;
     path.push_front(x);
+    std::cout << "pushed!" << std::endl;
+
+  }
+  std::cout << "PathTo 2" << std::endl;
   path.push_front(s);
   return path;
 }
