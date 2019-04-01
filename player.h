47ee7b002a02cf24eec7826cad6668572027f4b7
Jennifer Kulich <jennifer.kulich@mines.sdsmt.edu>
2019-04-01 15:11:00 -0600

    Deleted a ton of code that I wasn't using anymore as part of my strategy- Hamilton circuit code.

--- player.h
+++ player.h
@@ -10,9 +10,6 @@ class Player
 public:
    Player ();
    ValidMove makeMove(const Playfield *);
-private:
-  ValidMove rowsEven(int headX, int headY);
-  ValidMove rowsOdd(int headX, int headY, int foodX, int foodY);
 
 
 };
